5a166c5e63c900e74df5a064f0a656e4621ee3df
giorgixunda <145927422+giorgixunda@users.noreply.github.com>
2024-02-01 01:43:09 +0400

    Add files via upload

---
+++ day36/Homework/index.js
@@ -0,0 +1,8 @@
+console.log(5 + 5)
+num1 = window.prompt("enter first number")
+num2 = window.prompt("enter second number")
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
