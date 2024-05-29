8137cea8c60efd1a44308222806ff94e707a496e
giorgixunda <145927422+giorgixunda@users.noreply.github.com>
2024-05-29 21:59:26 +0400

    Delete day64 directory

--- day64/Homework/Homework2/script.js
+++
@@ -1,7 +0,0 @@
-const personName = String(prompt("enter your name"))
-const lastname = String(prompt("enter your last name"))
-window.alert(`Welcome ${personName} ${lastname}`)
-
-if(personName == "null" && lastname == "null"){
-    window.alert("please enter only name and lastname")
-}
