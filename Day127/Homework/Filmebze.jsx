54bdbe441deeac873083e5cf4c4a3d3edf0f46cb
giorgixunda <145927422+giorgixunda@users.noreply.github.com>
2024-12-12 01:39:49 +0400

    Add files via upload

---
+++ Day127/Homework/Filmebze.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Filmebze() {
+  return (
+    <div>
+      <h1>Filmebi</h1>
+      <p>Rambo</p>
+      <p>The GodFather</p>
+      <p>Etc....</p>
+    </div>
+  )
+}
---
+++ Day127/Homework/Serialebi.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Serialebi() {
+  return (
+    <div>
+      <h1>Serialebi</h1>
+      <p>Shua Qalaqshi</p>
+      <p>teen wolf</p>
+      <p>Qerchi</p>
+      <p>etc.....</p>
+    </div>
+  )
+}
